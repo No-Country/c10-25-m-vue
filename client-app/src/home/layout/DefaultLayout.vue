5764b31993a5c9696669087b776da2ffb1173bd5
kasnino <kasn.emprendedor@gmail.com>
2023-03-29 09:30:23 -0400

    Structure created with DefaultLayout component

---
+++ client-app/src/home/layout/DefaultLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <header>
+      <!-- header content goes here -->
+    </header>
+    <main>
+      <slot></slot>
+    </main>
+    <footer>
+      <!-- footer content goes here -->
+    </footer>
+  </div>
+</template>
+
+<script setup>
+export default {
+  name: "DefaultLayout",
+};
+</script>
