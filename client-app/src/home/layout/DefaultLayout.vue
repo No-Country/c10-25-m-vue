c84744477260fc4fbfc1dace314fc33b27c514fc
kasnino <kasn.emprendedor@gmail.com>
2023-03-29 14:58:24 -0400

    add Footer & Header Componentes

--- client-app/src/home/layout/DefaultLayout.vue
+++ client-app/src/home/layout/DefaultLayout.vue
@@ -2,18 +2,31 @@
   <div>
     <header>
       <!-- header content goes here -->
+      <Header></Header>
     </header>
     <main>
       <slot></slot>
     </main>
     <footer>
       <!-- footer content goes here -->
+      <Footer></Footer>
     </footer>
   </div>
 </template>
 
-<script setup>
-export default {
-  name: "DefaultLayout",
-};
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Header from "../components/Header.vue";
+import Footer from "../components/Footer.vue";
+export default defineComponent({
+  name: 'DefaultLayout',
+  components: {
+    Header,
+    Footer
+  },
+  setup() {
+    const message = 'This is my component!';
+    return { message };
+  },
+});
 </script>
