4cced2f52dc20ae7cbb547fd65dc059b5ab147e7
JaluGJ <gabriel.i.jalil@gmail.com>
2023-03-31 10:33:54 -0300

    feat(api_animalValidator): add /animal route to index

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import authRouter from './auth.routes';
+import animalRouter from './animal.routes'
 
 const router = Router();
 
 router.use('/auth', authRouter);
+router.use('/animal', animalRouter);
 
 export default router;
