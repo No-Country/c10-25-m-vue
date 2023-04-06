b2950f65677d7e427263717bc382cd6e06642bae
kasnino <kasn.emprendedor@gmail.com>
2023-04-06 09:35:38 -0400

    validation add

--- client-app/src/auth/components/RegisterForm.vue
+++ client-app/src/auth/components/RegisterForm.vue
@@ -6,37 +6,25 @@
                     alt="Login Image" />
             </div>
             <div class="login-form">
-                <img :src="imagenLogoRegister" alt="Logo" class="logo">
-
-                <form>
+                <LogoInterno/>
+                <div class="container__subtitle">
+                <h1>Registrate</h1>
+                <p>Completá tus datos para comenzar.</p>
+               </div>
+                <form @submit="onSubmit">
                     <div class="form-group">
-                        <div class="form-row">
-                            <label for="input1">Label 1</label>
-                            <input type="text" id="input1" />
-                        </div>
-                        <div class="form-row">
-                            <label for="input2">Label 2</label>
-                            <input type="text" id="input2" />
-                        </div>
-                        <div class="form-row">
-                            <label for="input3">Label 3</label>
-                            <input type="text" id="input3" />
-                        </div>
-                        <div class="form-row">
-                            <label for="input4">Label 4</label>
-                            <input type="text" id="input4" />
-                        </div>
-                        <div class="form-row">
-                            <label for="input5">Label 5</label>
-                            <input type="text" id="input5" />
-                        </div>
-                        <div class="form-row">
-                            <label for="input6">Label 6</label>
-                            <input type="text" id="input6" />
-                        </div>
-                    </div>
+                        <InputWithLabel class="form-row" placeholder="Nombre" :errors="errors.name" id="nombre" label="Nombre" v-model:value="name" />
+                        <InputWithLabel class="form-row" placeholder="Apellido" :errors="errors.surname" id="apellido" label="Apellido" v-model:value="surname" />
+                        <InputWithLabel class="form-row" placeholder="Email" :errors="errors.email" id="email" label="Email" v-model:value="email" />
+                        <InputWithLabel class="form-row" placeholder="Teléfono" :errors="errors.phone" id="telefono" label="Teléfono" v-model:value="phone" />
+                        <InputWithLabel class="form-row" placeholder="Contraseña" :errors="errors.password" id="contraseña" label="Contraseña" v-model:value="password" />
+                        <InputWithLabel class="form-row" placeholder="Repetir contraseña" :errors="errors.confirmPassword" id="repetirContraseña" label="Repetir contraseña" v-model:value="confirmPassword" />
 
+                        
+                    </div>
+                    <div class="container_btn">
                     <button type="submit">Registrare</button>
+                    </div>
                 </form>
 
             </div>
@@ -46,36 +34,115 @@
   
 <script  lang="ts">
 import { defineComponent, ref } from 'vue'
+import InputWithLabel from '../../shared/InputWithLabel.vue'
 import imagenPortadaRegister from '../../assets/auth/primer-plano-gato-lamiendo-oreja-conejo-aislado-blanco-removebg-preview 1.png';
-import imagenLogoRegister from '../../assets/auth/Veterinaria_logotipo-removebg-preview 1.png';
+import LogoInterno from '../../shared/LogoInterno.vue'
+import * as yup from "yup";
+import { useForm } from "vee-validate";
+
+interface FormValues {
+    name:string;
+    surname:string;
+    email: string;
+    phone: number;
+    password: string;
+    confirmPassword: string;
+}
+
+const schema = yup.object<FormValues>({
+  name: yup.string().required(),  
+  surname: yup.string().required(),  
+  email: yup.string().email().required('Email es requerido.'),
+  phone: yup.number().required(),  
+  password: yup.string().required('Contraseña incorrecta. Por favor, intentá de nuevo.').min(8, 'La contraseña debe tener más de 8 caracteres.'),
+  confirmPassword: yup.string()
+    .oneOf([yup.ref('password')], 'Las contraseñas deben coincidir')
+    .required('La confirmación de la contraseña es requerida')
+});
+
+
 export default defineComponent({
-    name: 'LoginForm',
+    name: 'RegisterForm',
+    components:{
+        LogoInterno,
+        InputWithLabel
+    },
     setup() {
-        const email = ref('')
-        const password = ref('')
 
-        function submitForm() {
-            // Lógica para enviar el formulario
-        }
 
+        const { useFieldModel, errors, handleSubmit, validate } = useForm<FormValues>({
+      validationSchema: schema,
+      validateOnInput: false
+    });
+
+        const [name, surname, email, phone, password, confirmPassword ] = useFieldModel([ 'name', 'surname', 'email', 'phone', 'password', 'confirmPassword']);
+        const onSubmit = handleSubmit((values) => {
+      alert(JSON.stringify(values, null, 2));
+    });
         return {
+            name,
+            surname,
             email,
+            phone,
             password,
+            confirmPassword,
+            validate,
+            errors,
             imagenPortadaRegister,
-            imagenLogoRegister,
-            submitForm
+            onSubmit
         }
     }
 })
 </script>
   
 <style scoped lang="scss">
+
+.container__subtitle{
+    display:flex;
+  
+flex-direction: column;
+line-height:25px;
+padding-top:5%;
+}
 .login-container {
     background-image: url(../../assets/auth/fonto_auth.png);
     background-size: cover;
     background-position: center;
 }
 
+.container_btn {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+  padding: 9px;
+}
+
+.login-form h1 {
+  font-family: 'Jost';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 33px;
+ 
+  /* identical to box height */
+  padding-top: 10px;
+
+  color: #060859;
+
+}
+
+.login-form p {
+    font-family: 'Jost';
+font-style: normal;
+font-weight: 400;
+width:100%;
+font-size: 16px;
+padding:10px;
+/* identical to box height, or 28px */
+
+
+color: #383B43;
+}
 .login-grid {
     display: grid;
     grid-template-columns: repeat(2, 1fr);
@@ -88,6 +155,8 @@ export default defineComponent({
     grid-template-rows: repeat(3, auto);
     gap: 10px;
     justify-content: center;
+    padding-top:15px;
+    
 }
 
 .login-image {
@@ -98,6 +167,8 @@ export default defineComponent({
 .login-form {
     display: flex;
     flex-direction: column;
+    padding: 10px;
+    justify-content: center;
     align-items: center;
 }
 
@@ -107,18 +178,12 @@ export default defineComponent({
     flex-direction: column;
 }
 
-.logo {
-    width: 100px;
-    height: 100px;
-}
-
 form {
     display: flex;
     flex-direction: column;
-
-
     text-align: left;
     gap: 10px;
+    width:90%;
 }
 
 .container__form--login {
@@ -128,19 +193,7 @@ form {
     align-items: center;
 }
 
-form label {
-    text-align: left;
 
-}
-
-form input {
-    padding: 8px 16px;
-    width: 200px;
-    height: 44px;
-    background: #FFFFFF;
-    border: 1px solid #3A57E8;
-    border-radius: 4px;
-}
 
 .form-options {
     display: flex;
@@ -176,6 +229,4 @@ input[type="checkbox"] {
 }
 
 
-
-.form-options p {}
 </style>
