a31eca986ef8fd99c9ef6379886d8507c5d59ed3
kasnino <kasn.emprendedor@gmail.com>
2023-04-06 02:04:03 -0400

    imagenes import assets - preparando production demo

--- client-app/src/auth/components/RegisterForm.vue
+++ client-app/src/auth/components/RegisterForm.vue
@@ -1,149 +1,155 @@
 <template>
     <div class="login-container">
-      <div class="login-grid">
-        <div class="login-image">
-          <img src="../../assets/auth/primer-plano-gato-lamiendo-oreja-conejo-aislado-blanco-removebg-preview 1.png" alt="Login Image" />
+        <div class="login-grid">
+            <div class="login-image">
+                <img :src="imagenPortadaRegister"
+                    alt="Login Image" />
+            </div>
+            <div class="login-form">
+                <img :src="imagenLogoRegister" alt="Logo" class="logo">
+
+                <form>
+                    <div class="form-group">
+                        <div class="form-row">
+                            <label for="input1">Label 1</label>
+                            <input type="text" id="input1" />
+                        </div>
+                        <div class="form-row">
+                            <label for="input2">Label 2</label>
+                            <input type="text" id="input2" />
+                        </div>
+                        <div class="form-row">
+                            <label for="input3">Label 3</label>
+                            <input type="text" id="input3" />
+                        </div>
+                        <div class="form-row">
+                            <label for="input4">Label 4</label>
+                            <input type="text" id="input4" />
+                        </div>
+                        <div class="form-row">
+                            <label for="input5">Label 5</label>
+                            <input type="text" id="input5" />
+                        </div>
+                        <div class="form-row">
+                            <label for="input6">Label 6</label>
+                            <input type="text" id="input6" />
+                        </div>
+                    </div>
+
+                    <button type="submit">Registrare</button>
+                </form>
+
+            </div>
         </div>
-        <div class="login-form">
-    <img src="../../assets/auth/Veterinaria_logotipo-removebg-preview 1.png" alt="Logo" class="logo">
-  
-    <form>
-        <div class="form-group">
-    <div class="form-row">
-      <label for="input1">Label 1</label>
-      <input type="text" id="input1" />
-    </div>
-    <div class="form-row">
-      <label for="input2">Label 2</label>
-      <input type="text" id="input2" />
-    </div>
-    <div class="form-row">
-      <label for="input3">Label 3</label>
-      <input type="text" id="input3" />
-    </div>
-    <div class="form-row">
-      <label for="input4">Label 4</label>
-      <input type="text" id="input4" />
-    </div>
-    <div class="form-row">
-      <label for="input5">Label 5</label>
-      <input type="text" id="input5" />
-    </div>
-    <div class="form-row">
-      <label for="input6">Label 6</label>
-      <input type="text" id="input6" />
-    </div>
-  </div>
-    
-      <button type="submit">Registrare</button>
-    </form>
-   
-  </div>
-      </div>
     </div>
 </template>
   
-  <script  lang="ts">
-  import { defineComponent, ref } from 'vue'
-  
-  export default defineComponent({
+<script  lang="ts">
+import { defineComponent, ref } from 'vue'
+import imagenPortadaRegister from '../../assets/auth/primer-plano-gato-lamiendo-oreja-conejo-aislado-blanco-removebg-preview 1.png';
+import imagenLogoRegister from '../../assets/auth/Veterinaria_logotipo-removebg-preview 1.png';
+export default defineComponent({
     name: 'LoginForm',
     setup() {
-      const email = ref('')
-      const password = ref('')
-  
-      function submitForm() {
-        // Lógica para enviar el formulario
-      }
-  
-      return {
-        email,
-        password,
-        submitForm
-      }
+        const email = ref('')
+        const password = ref('')
+
+        function submitForm() {
+            // Lógica para enviar el formulario
+        }
+
+        return {
+            email,
+            password,
+            imagenPortadaRegister,
+            imagenLogoRegister,
+            submitForm
+        }
     }
-  })
-  </script>
+})
+</script>
   
-  <style scoped lang="scss">
-  .login-container {
+<style scoped lang="scss">
+.login-container {
     background-image: url(../../assets/auth/fonto_auth.png);
     background-size: cover;
     background-position: center;
-  }
-  
-  .login-grid {
+}
+
+.login-grid {
     display: grid;
     grid-template-columns: repeat(2, 1fr);
     align-items: center;
-  }
-
-  .form-group {
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  grid-template-rows: repeat(3, auto);
-  gap: 10px;
-  justify-content: center;
 }
-  
-  .login-image {
+
+.form-group {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: repeat(3, auto);
+    gap: 10px;
+    justify-content: center;
+}
+
+.login-image {
     display: flex;
     justify-content: center;
-  }
-  
-  .login-form {
+}
+
+.login-form {
     display: flex;
     flex-direction: column;
     align-items: center;
-  }
-  
-  .form-row {
-  display: flex;
-  justify-content: center;
-flex-direction: column;
 }
 
-  .logo {
+.form-row {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+}
+
+.logo {
     width: 100px;
     height: 100px;
-  }
-  
-  form {
+}
+
+form {
     display: flex;
     flex-direction: column;
-   
-  
+
+
     text-align: left;
     gap: 10px;
-  }
-  
-  .container__form--login{
+}
+
+.container__form--login {
     display: flex;
     justify-content: center;
     align-content: center;
     align-items: center;
-  }
-  form label{
+}
+
+form label {
     text-align: left;
-    
-  }
-  
-  form input{
-  padding: 8px 16px;
-  width: 200px;
-  height: 44px;
-  background: #FFFFFF;
-  border: 1px solid #3A57E8;
-  border-radius: 4px;
-  }
-  .form-options {
+
+}
+
+form input {
+    padding: 8px 16px;
+    width: 200px;
+    height: 44px;
+    background: #FFFFFF;
+    border: 1px solid #3A57E8;
+    border-radius: 4px;
+}
+
+.form-options {
     display: flex;
     justify-content: space-between;
     width: 100%;
-  
-  }
-  
-  form button{
+
+}
+
+form button {
     background-color: #3A57E8;
     color: white;
     font-family: 'Jost', sans-serif;
@@ -157,21 +163,19 @@ flex-direction: column;
     font-style: normal;
     font-weight: 400;
     font-size: 26px;
-    margin-top:3%;
+    margin-top: 3%;
     cursor: pointer;
     color: #FFFFFF;
-    
-  }
-  
-  input[type="checkbox"] {
+
+}
+
+input[type="checkbox"] {
     /* styles for checkboxes */
     width: 20px;
     height: 20px;
-  }
-  
-  
-  
-  .form-options p{
-   
-  }
-  </style>
+}
+
+
+
+.form-options p {}
+</style>
