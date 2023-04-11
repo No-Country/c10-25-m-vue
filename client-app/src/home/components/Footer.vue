811b36cc4cb0d8ef96c168dfe528e95e1391328b
yamilaq <yamila_s_quinteros@hotmail.com>
2023-04-11 11:12:32 -0300

    footer responsive

--- client-app/src/home/components/Footer.vue
+++ client-app/src/home/components/Footer.vue
@@ -58,14 +58,16 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-
 .container-footer{
-    width: 100%;
+    width: max-content;
     background: var(--bg-footer);
     padding: 1em 1em 2em 1em;
-    display: grid;
-    grid-template-columns: 1fr 4fr 1fr 1fr;
-    justify-items: center;
+    display: inline-flex;
+    flex-direction: column;
+    justify-self: center;
+    
+
+    
     color: var(--text-footer);
     font-family: Jost;
     font-size: small;
@@ -73,24 +75,22 @@ export default defineComponent({
 }
 
 .logo_footer{
-    margin-left: 2rem;
+    margin: 1rem 12.6rem 2rem 12.6rem;
     display: flex;
     justify-content: center;
     align-items: center;
-
+    
 }
+
 .logo_footer img{
-    width: 8rem;
-    height: 8rem;
+    width: 3rem;
+    height: 3rem;
 }
-.titulo_footer{
-    width: 8rem;
-    height: 7rem;
-    margin-top: 3rem;
 
+.titulo_footer{
     display: block;
     align-self:center;
-
+    
 }
 .titulo_footer h2{
     color: var(--bg-btn);
@@ -101,22 +101,27 @@ export default defineComponent({
 .titulo_footer p{
     color: black;
     text-align: center;
-
-}
-.contacto{
-    margin:0.5rem 3rem 0 1.5rem;
-}
-.contacto h3{
-    width: 6rem;
-    text-align: center;
-    font-weight: 600;
-}
-.contacto_items{
-    padding: 1em;
-    display: flex;
-    gap: 1em ;
     
 }
+.contacto{
+    width: 100%;
+    margin: auto;
+}
+.contacto h3{
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: large;
+}
+.contacto_items{
+    width: min-content;
+    margin-left: 2rem;
+    margin-bottom: 1rem;
+    padding: 1em;
+    display: inline-flex;
+    gap: 1em   
+}
+
 .contacto_items ul{
     display: grid;
     row-gap: 1.5em;
@@ -126,42 +131,114 @@ export default defineComponent({
     text-align: left;
 
 }
+
 .seguinos{
-    margin:0.5rem 0 0 2em;
+    width: 100%;
+    margin: auto;
 }
 .seguinos h3{
-    width: 5rem;
+    
     font-weight: 600;
-    text-align: left;
+    font-size: large;
+    text-align: center;
 }
 .seguinos_items {
+    margin: 1rem 0 1rem 0;
     padding-top: 1em;
     display:flex;
     justify-content: center;
-    gap: 2em;
+    gap: 3em;
 }
+.pawprint_footer{
+    display: none;
+}
+@media (min-width: 618px){
+    .container-footer{
+        width: 50rem;
+    }
+    .logo_footer{
+        margin: 1rem 2rem 2rem 2rem;
+    }
+}
+@media (min-width: 900px) {
+    .container-footer{
+        width: 100%;
+    
+        display: grid;
+        grid-template-columns: 1fr 4fr 1fr 1fr;
+        justify-items: center;
+    }
 
-#paw1{
-    position: absolute;
-    top: 45%;
-    right: 9%;
+    .logo_footer{
+        margin-left: 2rem;    
+    }
+    .logo_footer img{
+        width: 8rem;
+        height: 8rem;
+    }
+    .titulo_footer{
+        width: 8rem;
+        height: 7rem;
+        margin-top: 3rem;
+        
+    }
+    .contacto{
+        margin:0.5rem 3rem 0 1.5rem;
+    }
+    .contacto h3{
+        width: 6rem;
+        text-align: center;
+        font-weight: 600;
+    }
+    .contacto_items{
+        width:auto;
+        margin: auto;
+        padding: 1em;
+        display: flex;
+        gap: 1em ;
+        
+    }
+
+    .seguinos{
+        width: auto;
+        margin:0.5rem 0 0 2em;
+    }
+    .seguinos h3{
+        width: 5rem;
+        font-weight: 600;
+        text-align: left;
+    }
+    .seguinos_items {
+        margin: auto;
+        gap: 2em;
+    }
+    .pawprint_footer{
+        display: block;
+    }
+    #paw1{
+        position: absolute;
+        top: 45%;
+        right: 9%;
+        
+    }
+    #paw2{
+        position: absolute;
+        top: 50%;
+        right: 4%;
+    
+    }
+    #paw3{
+        position: absolute;
+        top: 70%;
+        right: 9%;
+    
+    }#paw4{
+        position: absolute;
+        top: 75%;
+        right: 4%;
+        
+    }
     
 }
-#paw2{
-    position: absolute;
-    top: 50%;
-    right: 4%;
 
-}
-#paw3{
-    position: absolute;
-    top: 70%;
-    right: 9%;
-
-}#paw4{
-    position: absolute;
-    top: 75%;
-    right: 4%;
-    
-}
 </style>
