337a81e32a0ffcdc1e71899632d74a6e6de0f4eb
kasnino <kasn.emprendedor@gmail.com>
2023-04-13 03:14:15 -0400

    add type string JWT_EXPIRE, the ENV show value success SignIn token

--- api/src/utils/jwt.ts
+++ api/src/utils/jwt.ts
@@ -8,7 +8,7 @@ export const generateJWT = (id: number): Promise<string> => {
       payload,
       process.env.JWT_SECRET!,
       {
-        expiresIn: process.env.JWT_EXPIRE_IN,
+         expiresIn: process.env.JWT_EXPIRE as string,
       },
       (err, token) => {
         if (err) {
