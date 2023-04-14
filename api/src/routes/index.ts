bef311d4862ebc9a28f371bc3dbd9e1ef8a392b5
JaluGJ <gabriel.i.jalil@gmail.com>
2023-04-14 03:09:01 -0300

    feat: pets index route conection

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -4,12 +4,14 @@ import authRouter from './auth.routes';
 import animalRouter from './animal.routes'
 import userRouter from './user.routes';
 import appointmenRouter from './appointment.routes';
+import petRouter from './pets.routes'
 
 const router = Router();
 
 router.use('/auth', authRouter);
 router.use('/animals', animalRouter);
 router.use('/users', userRouter);
+router.use('/pets', petRouter)
 router.use('/appointments', appointmenRouter);
 
 export default router;
